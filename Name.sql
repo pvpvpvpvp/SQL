79bce42f18def2975c9c05aa020f18b78c397874
pvpvpvpvp <wmrqj@naver.com>
2021-08-02 15:32:50 +0900

    hr 계정 활성화 SQL set

---
+++ Name.sql
@@ -0,0 +1,3 @@
+ALTER SESSION SET "_ORACLE_SCRIPT" =TRUE;
+
+@?\demo\schema\human_resource\hr_main.sql
---
+++ new.sql
@@ -0,0 +1,2 @@
+ALTER SESSION SET "_ORACLE_SCRIPT" =TRUE
+@?\demo\schema\human_resource\hr_main.sql
