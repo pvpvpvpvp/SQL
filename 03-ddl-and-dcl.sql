09b413fb2a0b6f1cfa1852628044c16e2e2a3fa6
pvpvpvpvp <wmrqj@naver.com>
2021-08-06 11:43:52 +0900

    create 연습?

--- 03-ddl-and-dcl.sql
+++ 03-ddl-and-dcl.sql
@@ -35,5 +35,71 @@ SELECT * FROM DBA_USERS WHERE username = 'C##BITUSER';
 -- 시스템 권한의 부여: GRANT 권한 TO 사용자
 -- C##BITUSER에게 create session 권한을 부여
 GRANT create session TO C##BITUSER;
+GRANT create ROLE TO C##BITUSER;
+GRANT create TABLE TO C##BITUSER;
 
+GRANT connect, resource TO C##BITUSER;
+
+GRANT select ON HR.EMPLOYEES TO C##BITUSER;
 -- 일반적으로 CONNECT, RESOURCE 롤을 부여하면 일반사용자의 역할 수행 가능
+
+ALTER USER C##BITUSER           -- 사용자 정보 수정
+    DEFAULT TABLESPACE USERS    -- 기본 테이블 스페이스를 USERS 에 지정
+    QUOTA UNLIMITED ON USERS;
+--DDL
+--데이터 정의어 
+--이후는 C##BITWEEN
+
+-- 현재 내가 소유한 테이블 목록 확인
+SELECT * FROM tab;
+--현재 나에게 주어진 roll을 조회
+SELECT * FROM USER_ROLE_PRIVS;
+
+CREATE TABLE book(
+    book_id NUMBER(5),
+    title VARCHAR2(50),
+    author VARCHAR2(10),
+    pub_date DATE DEFAULT SYSDATE
+);
+
+SELECT * FROM tab;
+DESC book;
+
+--서브쿼리를 이용한 테이블 생성
+--hr스키마의 empolyees
+
+SELECT * FROM HR.employees;
+
+--job_id가 it 관련 직원들만 뽑아내어 새 테이블 생성
+
+CREATE TABLE it_emps AS(
+    SELECT * FROM hr.employees
+    WHERE job_id LIKE 'IT_%'
+    );
+DESC IT_EMPS;
+SELECT * FROM IT_EMPS;
+
+-- AUTHOR 테이블
+DROP TABLE AUTHOR;
+CREATE TABLE author(
+    author_id NUMBER(10),
+    author_name VARCHAR2(50) NOT NULL,
+    author_desc VARCHAR2(500),
+    PRIMARY KEY (author_id)
+);
+
+desc author;
+
+--book 테이블의 author 컬럼 지우기
+--나중에 author 테이블과 FK연결
+
+DESC   book;
+ALTER TABLE book DROP COLUMN author_ud;
+
+-- 테이블 참조를 위한 컬럼 추가 author_id
+ALTER TABLE book
+ADD (author_id NUMBER(10));
+
+-- 테이블의 book_id도 number(10) 으로..
+ALTER TABLE book
+MODIFY (book_id NUMBER(10));
--- p04.sql
+++ p04.sql
@@ -1,16 +1,113 @@
+--Q 1
 SELECT COUNT(*) 
 FROM employees 
 WHERE salary < (SELECT avg(salary) FROM employees);
+--Q 2
+SELECT employee_id, first_name, salary ,ROUND(t.avgSalary,2), t.maxSalary
+FROM employees e , (SELECT AVG(salary) avgSalary,
+                            MAX(salary) maxSalary
+                            FROM employees) t
+WHERE salary BETWEEN t.avgSalary AND t.maxSalary;
+  
+  
+ --3번 나누어 보기
+-- steven의 소속
+SELECT department_id FROM employees WHERE first_name='Steven' AND last_name='King';
+ 
+ -- 부서의 로케이션 아이디
+ SELECT location_id FROM departments
+ WHERE department_id = (SELECT department_id 
+                    FROM employees 
+                    WHERE first_name='Steven' 
+                    AND last_name='King');
+ 
+--3번
+ SELECT location_id, street_address, postal_code, city,state_province,country_id
+ FROM locations
+ WHERE location_id = (SELECT location_id FROM departments
+                        WHERE department_id = (SELECT department_id 
+                                                 FROM employees 
+                                                 WHERE first_name='Steven' 
+                                                 AND last_name='King'));
+   
+4번의 부분
+SELECT salary FROM employees WHERE job_id='ST_MAN';
 
-SELECT employee_id, first_name, salary
-FROM employees e
-WHERE salary >= (SELECT avg(salary) FROM employees)
-        AND salary <= (SELECT max(salary) FROM employees)
-        ORDER BY salary;
-        
- SELECT lo.location_id, street_address, postal_code, city,state_province,country_id
- FROM locations lo
- WHERE 'Steven' = (SELECT first_name FROM employees e,departments d WHERE e.department_id = d.department_id)
-     AND 'king' = (SELECT last_name FROM employees e,departments d WHERE e.department_id = d.department_id);
+--4번  
+SELECT employee_id,first_name,salary 
+FROM employees
+WHERE salary < ANY (SELECT salary FROM employees WHERE job_id='ST_MAN');
+
+5번 최고급여
+SELECT department_id, max(salary) FROM employees 
+GROUP BY department_id;
+5번
+SELECT employee_id , first_name, salary, department_id 
+FROM employees
+WHERE (department_id,salary) IN(SELECT department_id, max(salary) FROM employees 
+GROUP BY department_id);
+테이블 조인 버전
+SELECT employee_id , first_name, t.salary, e.department_id 
+FROM employees e ,(SELECT department_id, max(salary) salary FROM employees 
+GROUP BY department_id) t
+WHERE e.department_id = t.department_id AND e.salary = t.salary;
+
+6번 부분
+SELECT job_id , SUM(salary) FROM employees
+GROUP BY job_id; 
+6 번!
+SELECT job_title, t.sumSalary
+FROM jobs jd ,(SELECT job_id , SUM(salary) sumSalary FROM employees
+GROUP BY job_id) t
+WHERE jd.job_id = t.job_id;; 
+
+7번
+자신부서의 평균
+SELECT department_id, AVG(salary) avgSalary FROM employees
+GROUP BY department_id;
+7번의 최종
+SELECT e.employee_id, e.first_name , e.salary
+FROM employees e,(SELECT department_id, AVG(salary) avgSalary FROM employees
+                    GROUP BY department_id) t
+WHERE e.salary > t.avgsalary AND e.department_id =t.department_id;
+8번 문제~
+SELECT ROWNUM
+    employee_id,
+    first_name,
+    salary,
+    hire_date
+FROM employees
+ORDER BY hire_date asc;
                 
-                
+쿼리 2 
+SELECT ROWNUM rn,
+    employee_id,
+    first_name,
+    salary,
+    hire_date
+FROM (SELECT 
+    employee_id,
+    first_name,
+    salary,
+    hire_date
+FROM employees
+ORDER BY hire_date asc);
+
+SELECT rn,
+    employee_id,
+    first_name,
+    salary,
+    hire_date
+FROM (SELECT ROWNUM rn,
+    employee_id,
+    first_name,
+    salary,
+    hire_date
+FROM (SELECT 
+    employee_id,
+    first_name,
+    salary,
+    hire_date
+FROM employees
+ORDER BY hire_date asc))
+WHERE rn BETWEEN 11 AND 15;
